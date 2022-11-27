f1affe554f7b6cbc82fc0e528394906f083a8518
Key0just0Key <keysuwatcharapinun8@gmail.com>
2022-11-27 23:17:57 +0700

    sucessfully made the calculator BG and have started on buttons after failed research on invis button

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
 #code for the calculator's GUI
 from tkinter import * #import tkinter
 root = Tk()
-root.geometry("940x587")
+root.geometry("1200x750")
+#calculator as bg image 
 bg = PhotoImage(file = "C:\\Users\\Key\\Desktop\\PyProFE\\PyProjectFE\\Calculator.png")
+label = Label(root, image = bg)
+label.place(x = 0, y = 0)
 root.mainloop()
 
 #26/11/22: attempt to program RPN for calculator
